bdf79650543b9df0af4195626aa9a60c305fa4f8
Maria Rabelero <m.rabelero.campos@accenture.com>
2017-06-14 08:28:36 -0500

    header

--- src/scripts/front.js
+++ src/scripts/front.js
@@ -11,12 +11,13 @@ $(document).ready(function(){
     //Scrolling
     css3: false,
     scrollingSpeed: 400,
-    easing: 'swing',
-    easingcss3: 'ease',
+    easing: 'easeOutBack',
+    easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
     loopBottom: true,
 
     //Design
     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    verticalCentered: false,
 
   });
 });
