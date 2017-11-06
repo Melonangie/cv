5c512707aa9c43c9c47dbdddbd48544fa1c82722
Maria Rabelero <m.rabelero.campos@accenture.com>
2017-11-06 02:21:22 -0600

    update 2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,10 +3,17 @@
 // Main JS File
 $(document).ready(function() {
   $('#fullpage').fullpage({
-              anchors: ['home', 'services', 'about', 'work', 'contact'],
-              menu: '#menu',
-              slidesNavigation: true,
-              verticalCentered: true,
-              continuousVertical: true
+    //Navigation
+    anchors: ['home', 'services', 'about', 'work', 'contact'],
+    menu: '#menu',
+    slidesNavigation: true,
+    verticalCentered: true,
+    continuousVertical: true,
+
+    //Scrolling
+    easing: 'fade',
+
+    //Custom selectors
+    lazyLoading: true,
   });
 });
